e2ed0d316e614471b50cdc1395d68a65b29d1303
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-10-31 16:48:52 -0300

    feat: add self signed cert to app5

--- app4/vite.config.ts
+++ app4/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import fs from "node:fs";
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/app4/",
+import { URL } from "url";
+
+const prodServerOptions = {
+  https: {
+    key: fs.readFileSync(new URL("/etc/ssl/private/key.pem", import.meta.url)),
+    cert: fs.readFileSync(new URL("/etc/ssl/certs/cert.pem", import.meta.url)),
+  },
+};
+
+export default defineConfig(({ mode }: { mode: string }) => {
+  return {
+    plugins: [react()],
+    base: "/app4/",
+    server: mode === "production" ? prodServerOptions : {},
+  };
 });
